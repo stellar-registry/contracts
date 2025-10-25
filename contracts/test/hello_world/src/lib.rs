2bb774bf4dc0e282059c5302fe3582bed7d9aadd
ifropc <ifropc@apache.org>
2025-10-25 13:55:09 -0700

    feat!: refactor registry contract (#204)
    * WIP refactor resitry contract
    
    * Test fixes
    
    * Rollback stellar dir
    
    * Clippy
    
    * Bump versions
    
    * Fix name
    
    * clippy again
    
    * is clippy even useful
    
    * fix: storage should use a data key as not to overlap name spaces
    
    * feat: update to use main contracttraits PR and port storage
    
    This way the storage API can remain and be easier to upgrade once we have soroban-sdk- tools
    
    * feat: add events
    
    * feat: initial new tests
    
    * feat: organize tests and improve auth testing
    
    * feat: more tests and rename traits
    
    * Add HW test
    
    * Add test for publish hash
    
    * Add big test
    
    * fmt
    
    * Update authes
    
    * Update auth for dev deploy
    
    * clippy
    
    * Version test
    
    * Version test
    
    * fix: small clean up
    
    * fix: fmt
    
    ---------
    
    Co-authored-by: Willem Wyndham <willem@ahalabs.dev>

--- contracts/test/hello_world/src/lib.rs
+++ contracts/test/hello_world/src/lib.rs
@@ -1,24 +1,13 @@
 #![no_std]
-
 use admin_sep::{Administratable, Upgradable};
-use soroban_sdk::{contract, contractimpl, Address, Env};
-
-pub mod error;
-pub mod events;
-pub mod name;
-pub mod registry;
-mod util;
-pub mod version;
-
-mod storage;
-
-pub use error::Error;
+use soroban_sdk::{Address, Env, String, contract, contractimpl};
 
 #[contract]
 pub struct Contract;
 
 #[contractimpl]
 impl Administratable for Contract {}
+
 #[contractimpl]
 impl Upgradable for Contract {}
 
@@ -27,7 +16,7 @@ impl Contract {
     pub fn __constructor(env: &Env, admin: Address) {
         Self::set_admin(env, admin);
     }
+    pub fn hello(_: &Env, to: String) -> String {
+        to
+    }
 }
-
-#[cfg(test)]
-mod test;
